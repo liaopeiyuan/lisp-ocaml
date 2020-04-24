a95a89c1fbbc0d6e9e3ad2815d6e1b35d6154718
Alexander Liao <peiyuanl@andrew.cmu.edu>
2020-04-24 04:18:47 -0400

    basic environment; improved parsing; add

--- olisp.ml
+++ olisp.ml
@@ -1,3 +1,7 @@
+#load "str.cma";;
+
+let debug = false;
+
 (* Either *)
 type ('f, 's) e = | F of 'f | S of 's
 
@@ -14,15 +18,18 @@ type 'a scheme_obj =
     | Symbol : string -> symb scheme_obj
     | Number : (int64,float) e -> num scheme_obj
     | Atom : (symb scheme_obj, num scheme_obj) e -> atom scheme_obj
-    | List : 'a ele list -> l scheme_obj
+    | List : atom scheme_obj ele list -> l scheme_obj
     | Exp : (atom scheme_obj, l scheme_obj) e -> exp scheme_obj
-    | Env : (symb scheme_obj, 'b) Hashtbl.t -> env scheme_obj
+    | Env : (symb scheme_obj, store) Hashtbl.t -> env scheme_obj
+and store = 
+    | Proc of (l scheme_obj -> exp scheme_obj) 
+    | Ref of num scheme_obj
 
 let rec tokenize(chars: string) : string list = 
     let split = Str.split (Str.regexp "[ \t]+") in
-    let replacel = Str.global_replace (Str.regexp "[(]") "( " in
-    let replacer = Str.global_replace (Str.regexp "[)]") ") " in
-    chars |> replacel |> replacer |> split
+    let explode s = List.init (String.length s) (String.get s) in
+    let replace s = List.fold_left (fun x y -> let c = Char.escaped y in match c with "(" -> x^"( " | ")" -> x^" )"|_ -> x^c ) "" s in
+    chars |> explode |> replace |> split
 
 let atom (token: string) : atom scheme_obj = begin try
     let x = Int64.of_string token in Atom( S (Number (F x)))
@@ -33,7 +40,7 @@ let atom (token: string) : atom scheme_obj = begin try
         end
     end
 
-let rec pl (a: string list) b= match a with
+let rec pl (a: string list) b = if not(debug) then () else match a with
    | [] -> (Printf.printf "%s\n" b; ())
    | x::xs -> (Printf.printf "%s " (x) ; pl xs b)
 
@@ -45,13 +52,13 @@ let rec read_from_tokens (tokens: string list ref) : 'a ele =
         let result = ref [] in
         let () = tokens := xs in
         (
-        while (List.hd(!tokens) <> ")") do
-            result := ((read_from_tokens(tokens)))::(!result)
-        done; 
-        tokens := List.tl (!tokens); 
-        L (!result |> List.rev) 
+            while (List.hd(!tokens) <> ")") do
+                result := ((read_from_tokens(tokens)))::(!result)
+            done; 
+            tokens := List.tl (!tokens); 
+            L (!result |> List.rev) 
         )
-   | ")"::_ -> failwith "unexpected" 
+   | ")"::_ -> failwith "unexpected )" 
    | x::xs->  let () = tokens := xs in E (atom(x))
 
 let break(e : 'a ele) : 'a ele list = match e with
@@ -63,15 +70,37 @@ let parse(program:string) : exp scheme_obj =
     let result = input |> read_from_tokens |> break in
     Exp(S (List (result)) )
 
-let parse2(program:string) : 'a ele list =
+let _parse(program:string) : 'a ele list =
     let input = program |> tokenize |> ref in
     let result = input |> read_from_tokens |> break in
     result
 
-let program = "(begin (define r 10 ) (* pi (* r r )))"
+let literal x = Exp (F (Atom (S (Number (F x))))) 
 
-let res = tokenize(program)
-let res = parse2(program) 
+let basic_environment (_: unit) : env scheme_obj =
+    let hs: (symb scheme_obj, store) Hashtbl.t = Hashtbl.create 42 in
+    let rec add l = match l with
+      | List [] -> literal 0L
+      | List (  (E (Atom (S (Number (F a)))) )::xs  ) -> match add(List xs) with
+            | Exp (F (Atom (S (Number (F sum))))) ->  literal (Int64.add sum a)
+            | _ -> failwith "incorrect usage of add"
+    in
+    (   
+        Hashtbl.add hs (Symbol "+") (Proc add);
+        Env hs
+    )
+
+(* To Be Implemented *)
+let eval (expr) = expr
+
+let Env environ = basic_environment()
+let Proc f = Hashtbl.find environ (Symbol "+")
+let g x = (E (Atom (S (Number (F x)))) )
+
+let program = "(+ 1 2)"
+
+let a = tokenize(program)
+
+let res = _parse(program) 
 let e1::b = res
 let e2::d = b
-let e3::f = d
