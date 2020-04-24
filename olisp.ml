906af9d31c4199d5389f1e5ceb9a19a1c9e7b5f3
Alexander Liao <peiyuanl@andrew.cmu.edu>
2020-04-24 05:48:43 -0400

    update basic environment: add, quote, pi, list

--- olisp.ml
+++ olisp.ml
@@ -1,9 +1,7 @@
-#load "str.cma";;
-
 let debug = false;
 
 (* Either *)
-type ('f, 's) e = | F of 'f | S of 's
+type ('f, 's) e = | L of 'f | R of 's
 
 type symb
 type num
@@ -12,7 +10,7 @@ type l
 type exp
 type env
 
-type 'a ele = E of 'a | L of 'a ele list
+type 'a ele = Ele of 'a | Li of 'a ele list
 
 type 'a scheme_obj =
     | Symbol : string -> symb scheme_obj
@@ -32,20 +30,20 @@ let rec tokenize(chars: string) : string list =
     chars |> explode |> replace |> split
 
 let atom (token: string) : atom scheme_obj = begin try
-    let x = Int64.of_string token in Atom( S (Number (F x)))
+    let x = Int64.of_string token in Atom( R (Number (L x)))
       with Failure _ -> 
         begin try 
-            let y = Float.of_string token in Atom (S (Number (S y)))
-          with Failure _ -> Atom(F (Symbol token) )
+            let y = Float.of_string token in Atom (R (Number (R y)))
+          with Failure _ -> Atom(L (Symbol token) )
         end
     end
 
-let rec pl (a: string list) b = if not(debug) then () else match a with
+let rec print_list (a: string list) b = if not(debug) then () else match a with
    | [] -> (Printf.printf "%s\n" b; ())
-   | x::xs -> (Printf.printf "%s " (x) ; pl xs b)
+   | x::xs -> (Printf.printf "%s " (x) ; print_list xs b)
 
 let rec read_from_tokens (tokens: string list ref) : 'a ele = 
-    let () = pl (!tokens) "list" in
+    let () = print_list (!tokens) "list" in
     match !tokens with
    | [] -> failwith "unexptected EOF"
    | "("::xs -> 
@@ -56,51 +54,78 @@ let rec read_from_tokens (tokens: string list ref) : 'a ele =
                 result := ((read_from_tokens(tokens)))::(!result)
             done; 
             tokens := List.tl (!tokens); 
-            L (!result |> List.rev) 
+            Li (!result |> List.rev) 
         )
    | ")"::_ -> failwith "unexpected )" 
-   | x::xs->  let () = tokens := xs in E (atom(x))
+   | x::xs->  let () = tokens := xs in Ele (atom(x))
 
 let break(e : 'a ele) : 'a ele list = match e with
-    | L a -> a
-    | E _ -> failwith "incorrect usage of function"
+    | Li a -> a
+    | Ele _ -> failwith "incorrect usage of function"
 
 let parse(program:string) : exp scheme_obj =
     let input = program |> tokenize |> ref in
     let result = input |> read_from_tokens |> break in
-    Exp(S (List (result)) )
+    match result with 
+       | [] -> Exp (R (List []))
+       | (Ele x)::[] -> Exp(L (x))
+       | _ -> Exp(R (List (result)) )
 
 let _parse(program:string) : 'a ele list =
     let input = program |> tokenize |> ref in
     let result = input |> read_from_tokens |> break in
     result
 
-let literal x = Exp (F (Atom (S (Number (F x))))) 
+let literal x = Exp (L (Atom (R (Number (L x))))) 
+let float_literal x = Exp (L (Atom (R (Number (R x))))) 
+let to_hs (s) : (symb scheme_obj, store) Hashtbl.t = match s with (Env hs) -> hs 
 
 let basic_environment (_: unit) : env scheme_obj =
     let hs: (symb scheme_obj, store) Hashtbl.t = Hashtbl.create 42 in
     let rec add l = match l with
       | List [] -> literal 0L
-      | List (  (E (Atom (S (Number (F a)))) )::xs  ) -> match add(List xs) with
-            | Exp (F (Atom (S (Number (F sum))))) ->  literal (Int64.add sum a)
-            | _ -> failwith "incorrect usage of add"
+      | List (  (Ele (Atom (R (Number (L a)))) )::xs  ) -> (match add(List xs) with
+            | Exp (L (Atom (R (Number (L sum))))) ->  literal (Int64.add sum a)
+            | Exp (L (Atom (R (Number (R sum))))) ->  float_literal (Int64.to_float(a) +. sum)
+            | _ -> failwith "incorrect usage of add")
+      | List (  (Ele (Atom (R (Number (R a)))) )::xs  ) -> (match add(List xs) with
+            | Exp (L (Atom (R (Number (R sum))))) ->  float_literal (sum +. a)
+            | Exp (L (Atom (R (Number (L sum))))) ->  float_literal ( Int64.to_float(sum) +. a)
+            | _ -> failwith "incorrect usage of add")
+      | _ -> failwith "incorrect usage of add"
     in
+    let pi = Ref (Number (R Float.pi)) in
+    let quote = Proc (fun x -> match x with 
+                                | List [] -> failwith "Invalid quote: (quote) []" 
+                                | List (y::_) -> match y with 
+                                                | Ele e -> Exp (L e) 
+                                                | Li es -> Exp (R (List es)) ) 
+    in
+    let listify = Proc (fun x -> Exp (R x)) in
     (   
         Hashtbl.add hs (Symbol "+") (Proc add);
+        Hashtbl.add hs (Symbol "pi") pi;
+        Hashtbl.add hs (Symbol "quote") quote;
+        Hashtbl.add hs (Symbol "list") listify;
         Env hs
     )
 
 (* To Be Implemented *)
-let eval (expr) = expr
+let eval (e: exp scheme_obj) (env: env scheme_obj) = failwith "NotImplemented"
 
-let Env environ = basic_environment()
-let Proc f = Hashtbl.find environ (Symbol "+")
-let g x = (E (Atom (S (Number (F x)))) )
+let environ = basic_environment()
+let Env env = environ
+let Proc f = Hashtbl.find env (Symbol "+")
+let g x = (Ele (Atom (R (Number (L x)))) )
+let g2 x = (Ele (Atom (R (Number (R x)))) )
 
-let program = "(+ 1 2)"
+let c = (List [ Li [ Li [Ele( Atom(L(Symbol "+")))]; Ele( Atom(L(Symbol "+"))) ]; Ele( Atom(L(Symbol "+")))  ])
+let d = (List [ Li [ Li [Ele( Atom(L(Symbol "+")))]; Ele( Atom(L(Symbol "+"))) ] ])
+
+let program = "(define (merge L M) (if (null? L) M (if (null? M) L (if (< (car L) (car M)) (cons (car L) (merge (cdr L) M)) (cons (car M) (merge (cdr M) L))))))"
+let program = "(pi)"
 
 let a = tokenize(program)
 
 let res = _parse(program) 
-let e1::b = res
-let e2::d = b
+let res2 = parse(program) 
